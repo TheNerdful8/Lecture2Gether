051986d108093cae8f172b34c71c7d6a2aaa82c3
Finn-Thorben Sell <dev@finn-thorben.me>
2020-04-24 19:14:59 +0200

    adapt js project configs

--- lecture2gether-vue/.eslintrc.js
+++ lecture2gether-vue/.eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['warn', 4, {
+            FunctionExpression: { parameters: 'first' },
+        }],
+        quotes: ['warn', 'single'],
+    },
 };
--- lecture2gether-vue/vue.config.js
+++ lecture2gether-vue/vue.config.js
@@ -1,3 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
-  lintOnSave: false,
+    lintOnSave: false,
+    css: {
+        loaderOptions: {
+            sass: {
+                sassOptions: {
+                    includePaths: [path.resolve(__dirname, 'node_modules'), path.resolve(__dirname)],
+                },
+            },
+        },
+    },
 };
