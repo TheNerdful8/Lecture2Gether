e87728e140f5f926c33b4505edcfc4e1b34d41fa
Florian <florian@flova.de>
2020-04-26 00:07:33 +0200

    Fix tests

--- lecture2gether_flask/testrun.py
+++ lecture2gether_flask/testrun.py
@@ -16,12 +16,17 @@ def socketio_test():
     
     assert r.status_code == 400, f"Check failed, status code as {r.status_code}, but 400 was expected."
 
-    r = flask_test_client.post('/l2go', data={
+    r = flask_test_client.post('/l2go', json={
+        'username': 'python', 'password': 'is-great!'})
+    
+    assert r.status_code == 400, f"Check failed, status code as {r.status_code}, but 400 was expected."
+
+    r = flask_test_client.post('/l2go', json={
         'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/v/27934', 'password': ''})
 
     assert r.status_code == 200, f"Check failed, status code as {r.status_code}, but 200 was expected."
 
-    r = flask_test_client.post('/l2go', data={
+    r = flask_test_client.post('/l2go', json={
         'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/v/27934'})
 
     assert r.status_code == 200, f"Check failed, status code as {r.status_code}, but 200 was expected."
