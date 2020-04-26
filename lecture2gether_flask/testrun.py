2360a145ffaa1719bab90c7ed0c2e70ada8253a9
Florian <florian@flova.de>
2020-04-26 16:33:07 +0200

    Use other video in ci

--- lecture2gether_flask/testrun.py
+++ lecture2gether_flask/testrun.py
@@ -14,28 +14,23 @@ def socketio_test():
     # Hopefully fix weird ci behaviour
     time.sleep(1)
 
-    r = flask_test_client.post('/l2go', json={
+    r = flask_test_client.post('/api/l2go', json={
         'username': 'python', 'password': 'is-great!'})
     
     assert r.status_code == 400, f"Check failed, status code as {r.status_code}, but 400 was expected."
 
-    r = flask_test_client.post('/l2go', data={
-        'username': 'python', 'password': 'is-great!'})
-    
-    assert r.status_code == 404, f"Check failed, status code as {r.status_code}, but 400 was expected."
-
-    r = flask_test_client.post('/l2go', json={
+    r = flask_test_client.post('/api/l2go', data={
         'username': 'python', 'password': 'is-great!'})
     
     assert r.status_code == 400, f"Check failed, status code as {r.status_code}, but 400 was expected."
 
-    r = flask_test_client.post('/l2go', json={
-        'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/v/27934', 'password': ''})
+    r = flask_test_client.post('/api/l2go', json={
+        'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/l/4577', 'password': ''})
 
     assert r.status_code == 200, f"Check failed, status code as {r.status_code}, but 200 was expected."
-
-    r = flask_test_client.post('/l2go', json={
-        'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/v/27934'})
+    
+    r = flask_test_client.post('/api/l2go', json={
+        'video_url': 'https://lecture2go.uni-hamburg.de/l2go/-/get/l/4577'})
 
     assert r.status_code == 200, f"Check failed, status code as {r.status_code}, but 200 was expected."
 
