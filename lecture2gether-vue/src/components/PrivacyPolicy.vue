9fdd6bb657f9f617b7f5da286932ecf091b181e8
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-06-05 23:01:19 +0200

    Format privacy policy

--- lecture2gether-vue/src/components/PrivacyPolicy.vue
+++ lecture2gether-vue/src/components/PrivacyPolicy.vue
@@ -35,7 +35,6 @@
 
                     <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
                     <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
-                    <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
 
                     <h2>How we use your information</h2>
 
@@ -46,14 +45,13 @@
                     <li>Understand and analyze how you use our webste</li>
                     <li>Develop new products, services, features, and functionality</li>
                     <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the webste, and for marketing and promotional purposes</li>
-                    <li>Send you emails</li>
                     <li>Find and prevent fraud</li>
                     </ul>
                     </p>
 
                     <h2>Log Files</h2>
 
-                    <p>Lecture2Gether follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info">Privacy Policy Generator</a> and the <a href="https://www.privacypolicyonline.com/privacy-policy-generator/">Online Privacy Policy Generator</a>.</p>
+                    <p>Lecture2Gether follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
 
                     <h2>Cookies and Web Beacons</h2>
 
@@ -101,6 +99,8 @@
                     <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
                     <p>Lecture2Gether does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
+                    <p>Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info">Privacy Policy Generator</a> and the <a href="https://www.privacypolicyonline.com/privacy-policy-generator/">Online Privacy Policy Generator</a>.</p>
                 </v-card-text>
 
                 <v-divider></v-divider>
