983ce6dff3a6c929ec74683533be9e93dc67881e
Frederico Bormann <github@frederico.info>
2020-06-05 16:36:11 +0200

    Add footer and GitHub link

--- lecture2gether-vue/src/App.vue
+++ lecture2gether-vue/src/App.vue
@@ -4,6 +4,12 @@
             <Toolbar :collapsed="isCollapsed"></Toolbar>
             <router-view/>
         </v-content>
+        <v-footer>
+            <v-spacer></v-spacer>
+            <img src="/img/icons/GitHub.svg" alt="GitHub logo" class="mr-2"/>
+            <a href="https://github.com/TheNerdful8/Lecture2Gether" class="footer-link">Lecture2Gether on Github</a>
+            <v-spacer></v-spacer>
+        </v-footer>
     </v-app>
 </template>
 
@@ -54,5 +60,17 @@ export default class App extends Vue {
                 color: #42b983;
             }
         }
+    }
+
+    .footer-link {
+        color: #222222!important;
+    }
+
+    .footer-link:link {
+        text-decoration: none;
+    }
+
+    .footer-link:hover {
+        text-decoration: underline;
     }
 </style>
