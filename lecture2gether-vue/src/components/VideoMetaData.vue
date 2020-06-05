cc7853b24f021ffb01a1c53637c72b771dc00cfd
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-06-05 21:32:30 +0200

    Frontend: Display metadata WIP

--- lecture2gether-vue/src/components/VideoMetaData.vue
+++ lecture2gether-vue/src/components/VideoMetaData.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class="title">
-        {{ videoMetaData.Title }}
+    <div class="video-meta-data">
+        <div class="title">
+            {{ videoMetaData.Title }}
+        </div>
+        <a class="creator" :href="videoMetaData.CreatorLink">
+            {{ videoMetaData.Creator }}
+        </a>
+        <div class="date">
+            {{ videoDate }}
+        </div>
+        <a class="license" :href="videoMetaData.LicenseLink">
+            License: {{ videoMetaData.License }}
+        </a>
     </div>
 </template>
 
@@ -17,6 +28,11 @@ export default class VideoMetaData extends Vue {
 
     get videoMetaData() {
         return this.$store.state.player.videoMetaData;
+    }
+
+    get videoDate() {
+        const date = new Date(Date.parse(this.$store.state.player.videoMetaData.Date));
+        return date.toLocaleDateString();
     }
 }
 </script>
