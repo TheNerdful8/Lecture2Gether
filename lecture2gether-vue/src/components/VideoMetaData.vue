4761a0987da3a85e4a9d5900ea4db88c83bca10c
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-06-05 16:37:27 +0200

    Init VideoMetaData component

---
+++ lecture2gether-vue/src/components/VideoMetaData.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Component from 'vue-class-component';
+import Vue from 'vue';
+import { Watch } from 'vue-property-decorator';
+
+@Component({
+    components: { videoMetaData },
+})
+export default class VideoMetaData extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>
