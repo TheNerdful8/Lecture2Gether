41a1668ed2bddb46aa0c2324258e0c0a57d176b5
Finn-Thorben Sell <dev@finn-thorben.me>
2020-07-14 18:57:07 +0200

    allow cors from npm serve

--- lecture2gether-vue/vue.config.js
+++ lecture2gether-vue/vue.config.js
@@ -18,4 +18,14 @@ module.exports = {
     transpileDependencies: [
         'vuetify',
     ],
+
+    configureWebpack: {
+        devServer: {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+                'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+            },
+        },
+    },
 };
